fe5c960ebf2d5f76cef0afbdaa322739b86f4fe8
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-05-20 02:05:19 -0400

    textarea created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
-$("#currentDay").append(moment().format("dddd, MMMM Do"));
+var date = moment().format("dddd, MMMM Do");
+$("#currentDay").append(date);
+tasks = {};
 
 console.log($("#currentDay"), moment().format("H"), moment().format("dddd, MMMM Do"));
 
 var scheduleFormat = function() {
+    //Check each .row class
     $(".row").each(function() {
-        console.log($(this).attr("id"));
+      //  //console.log($(this).attr("id"));
+        //Check the row if to pick up the row hour >> to integer
         var hour = parseInt($(this).attr("id").replace("hour-",""));
-        console.log(hour, typeof(hour), typeof(moment().format("H")));
+      //  //console.log(hour, typeof(hour), typeof(moment().format("H")));
+        //Check the current hour and colour the box accordingly
         if (parseInt(moment().format("H")) > hour) {
             $(this).find(".col-10").addClass("past");
         }
@@ -18,4 +23,18 @@ var scheduleFormat = function() {
         }
     });
 };
+$(".col-10").on("click", function(event) {
+  //  console.log($(this).find($("p")));
+  //  console.log(event);
+ //  console.log($(this).has("p").length)
+  //  console.log($("p").length)
+    var text = $(this).find("p")
+            .text()
+            .trim();
+    var textInput = $("<textarea>")
+            .val(text);
+        
+    $(this).find("p").replaceWith(textInput);
+    textInput.trigger("focus");
+});
 scheduleFormat();
